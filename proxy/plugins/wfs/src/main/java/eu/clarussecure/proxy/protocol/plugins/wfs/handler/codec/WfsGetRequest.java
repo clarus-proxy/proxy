1d337a8d4bbd8bf7e14f5aaec43dd99413acefe6
Thierry Chevallier <thierry.chevallier@akka.eu>
2017-07-10 11:29:03 +0200

    get and post requests

---
+++ proxy/plugins/wfs/src/main/java/eu/clarussecure/proxy/protocol/plugins/wfs/handler/codec/WfsGetRequest.java
@@ -0,0 +1,68 @@
+package eu.clarussecure.proxy.protocol.plugins.wfs.handler.codec;
+
+import eu.clarussecure.proxy.protocol.plugins.wfs.exception.WfsParsingException;
+import eu.clarussecure.proxy.protocol.plugins.wfs.parser.WfsParserUtil;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+/**
+ * Created on 04/07/2017.
+ */
+public class WfsGetRequest extends WfsRequest {
+
+    protected Map<String, String> wfsParams;
+    protected WfsOperation wfsOperation;
+
+    public WfsGetRequest(HttpVersion httpVersion, HttpMethod method, String uri) throws WfsParsingException {
+        super(httpVersion, method, uri);
+        setWfsParams();
+    }
+
+    public boolean hasMultipleParameters() {
+        return (this.uri().indexOf("&") != -1);
+    }
+
+    public boolean isWfsRequest() {
+
+        for (Map.Entry<String, String> entry : wfsParams.entrySet()) {
+            if (entry.getKey().equals(WfsParameter.SERVICE.getParameter())) {
+                return (entry.getValue().equalsIgnoreCase("WFS"));
+            }
+        }
+        return false;
+    }
+
+    public void setWfsParams() throws WfsParsingException {
+        try {
+            wfsParams = WfsParserUtil.parseUrl(this);
+
+        } catch (MalformedURLException e) {
+            // handle the exception locally
+            e.printStackTrace();
+
+        } catch (URISyntaxException e) {
+            // handle the exception locally
+            e.printStackTrace();
+        }
+    }
+
+    public WfsOperation getWfsOperation() throws WfsParsingException {
+
+        for (Map.Entry<String, String> entry : wfsParams.entrySet()) {
+            if (entry.getKey().equals(WfsParameter.REQUEST.getParameter())) {
+                return WfsOperation.valueOfByName(entry.getValue());
+            }
+        }
+        throw new WfsParsingException("No WFS operation found");
+    }
+
+    public Map<String, String> getWfsParams() {
+
+        return wfsParams;
+    }
+
+}
---
+++ proxy/plugins/wfs/src/main/java/eu/clarussecure/proxy/protocol/plugins/wfs/handler/codec/WfsPostRequest.java
@@ -0,0 +1,16 @@
+package eu.clarussecure.proxy.protocol.plugins.wfs.handler.codec;
+
+import eu.clarussecure.proxy.protocol.plugins.wfs.exception.WfsParsingException;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+
+/**
+ * Created on 04/07/2017.
+ */
+public class WfsPostRequest extends WfsRequest {
+
+    public WfsPostRequest(HttpVersion httpVersion, HttpMethod method, String uri) throws WfsParsingException {
+        super(httpVersion, method, uri);
+    }
+
+}
