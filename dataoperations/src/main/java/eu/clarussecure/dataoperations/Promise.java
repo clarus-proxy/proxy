c836197a989dd566ad4c086aa7f2943c06904c8e
fbrouille <f.brouille@akka.eu>
2017-09-08 14:54:49 +0200

    remove old projects (dataoperations + anonmodule)

--- dataoperations/src/main/java/eu/clarussecure/dataoperations/Promise.java
+++
@@ -1,8 +0,0 @@
-package eu.clarussecure.dataoperations;
-
-public interface Promise {
-    public int getId();
-    public String[] getAttributeNames();
-    public String getOperation();
-    public String[][] getCall();
-}
