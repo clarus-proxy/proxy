e0f4ccc93fb641581ca22958c8d2461cb1489c3c
hargathor <romain.ferrari@gmail.com>
2017-10-25 16:31:52 +0000

    Updating clarus starting script in order to update the mongodb endpoint using two environment variable. Issue #12

--- docker/configuration/clarus-proxy.sh
+++ docker/configuration/clarus-proxy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 source /etc/clarus/clarus-proxy.conf
+sed -i "s/EMBEDDED_MONGO_DB_IP/$EMBEDDED_MONGO_DB_IP/g" /etc/clarus/clarus-keystore.conf
+sed -i "s/EMBEDDED_MONGO_DB_PORT/$EMBEDDED_MONGO_DB_PORT/g" /etc/clarus/clarus-keystore.conf
+
 CLARUS_PROXY_VERSION=1.0.1
 
-$JAVA_COMMAND -DEMBEDDED_MONGO_DB=$EMBEDDED_MONGO_DB -Djava.ext.dirs=$CLARUS_LIBRARIES_PATH:$JAVA_HOME/lib/ext -jar $CLARUS_BASE_PATH/proxy-main-"$CLARUS_PROXY_VERSION".jar -sp "$CLARUS_SECURITY_POLICY" "$CLARUS_CLOUD_IP":"$CLARUS_CLOUD_PORT"
+$JAVA_COMMAND -Djava.ext.dirs=$CLARUS_LIBRARIES_PATH:$JAVA_HOME/lib/ext -jar $CLARUS_BASE_PATH/proxy-main-"$CLARUS_PROXY_VERSION".jar -sp "$CLARUS_SECURITY_POLICY" "$CLARUS_CLOUD_IP":"$CLARUS_CLOUD_PORT"
 
